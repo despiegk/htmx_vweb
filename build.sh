3a6d67bf8d7fb8797136307b39e3fb9dcb9310bd
timurgordon <timurgordon@gmail.com>
2022-08-19 18:06:17 +0300

    set up test buttons

--- build.sh
+++ build.sh
@@ -1,31 +1,34 @@
 echo "Starting build..."
 
-# TODO: Check if current version is latest to avoid redundant installation
-if [[ -f "tailwindcss" ]]
+if [[ ! -f "htmx.min.js" ]]
 then
-    rm tailwindcss
+    echo "Installing htmx..."
+    curl -sLO https://unpkg.com/htmx.org/dist/htmx.min.js
 fi
 
-# checks os and architecture for correct release
-# https://stackoverflow.com/a/8597411 
-echo "Installing & building tailwind..."
-ASSET="tailwindcss"
+# TODO: Check other paths for binary executable ?
+if [[ ! -f "tailwindcss" ]]
+then
+    # checks os and architecture for correct release
+    # https://stackoverflow.com/a/8597411 
+    echo "Installing & building tailwind..."
+    ASSET="tailwindcss"
 
-if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    ASSET="$ASSET-linux"
-elif [[ "$OSTYPE" == "darwin"* ]]; then
-    ASSET="$ASSET-macos"
+    if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        ASSET="$ASSET-linux"
+    elif [[ "$OSTYPE" == "darwin"* ]]; then
+        ASSET="$ASSET-macos"
+    fi
+    if [[ "$(uname -m)" == "x86_64"* ]]; then
+        ASSET="$ASSET-x64"
+    elif [[ "$(uname -m)" == "arm64"* ]]; then
+        ASSET="$ASSET-arm64"
+    fi
+
+    curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${ASSET}"
+    chmod +x $ASSET
+    mv $ASSET tailwindcss        
 fi
-if [[ "$(uname -m)" == "x86_64"* ]]; then
-    ASSET="$ASSET-x64"
-elif [[ "$(uname -m)" == "arm64"* ]]; then
-    ASSET="$ASSET-arm64"
-fi
-
-curl -sLO "https://github.com/tailwindlabs/tailwindcss/releases/latest/download/${ASSET}"
-chmod +x $ASSET
-mv $ASSET tailwindcss
-
 
 # initialized and configures tailwind if not configured
 echo "Initializing tailwind..."
